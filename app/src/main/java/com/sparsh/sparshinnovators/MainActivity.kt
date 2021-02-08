b36e23e24222d8bff9c513dcc9d1735dbd9dc88e
Saurabh Panday <saurabh@sparshmail.com>
2021-02-08 15:37:18 +0530

    mind sdk changed

--- app/src/main/java/com/sparsh/sparshinnovators/MainActivity.kt
+++ app/src/main/java/com/sparsh/sparshinnovators/MainActivity.kt
@@ -2,11 +2,13 @@ package com.sparsh.sparshinnovators
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aparsh.utils.SparshUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+      
     }
 }
